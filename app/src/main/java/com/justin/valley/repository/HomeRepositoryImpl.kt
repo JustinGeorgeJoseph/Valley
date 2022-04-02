d146088d25c8419ee6eccb7450a77c6268c09376
JustinGeorgeJoseph <justin@entri.me>
2022-04-02 23:35:31 +0530

    Combine api response to single source

--- app/src/main/java/com/justin/valley/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/justin/valley/repository/HomeRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package com.justin.valley.repository
 
-import android.util.Log
 import com.justin.valley.domain.repo.HomeRepository
 import com.justin.valley.models.categories.Category
 import com.justin.valley.models.channels.Channel
 import com.justin.valley.models.newepisodes.Media
-import com.justin.valley.utils.TAG_LOG
 import retrofit2.Retrofit
 import javax.inject.Inject
 
@@ -16,7 +14,6 @@ class HomeRepositoryImpl @Inject constructor(private val retrofit: Retrofit) : H
     override suspend fun getNewEpisodes(): List<Media>? {
         try{
             val response = calls.getNewEpisodes()
-            Log.d(TAG_LOG,response.toString())
             return response.data?.media
         } catch (e: Exception){
             throw e
@@ -26,7 +23,6 @@ class HomeRepositoryImpl @Inject constructor(private val retrofit: Retrofit) : H
     override suspend fun getChannels(): List<Channel>? {
         try{
             val response = calls.getChannels()
-            Log.d(TAG_LOG,response.toString())
             return response.data?.channels
         } catch (e: Exception){
             throw e
@@ -35,7 +31,6 @@ class HomeRepositoryImpl @Inject constructor(private val retrofit: Retrofit) : H
     override suspend fun getCategories(): List<Category>? {
         try{
             val response = calls.getCategories()
-            Log.d(TAG_LOG,response.toString())
             return  response.data?.categories
         } catch (e: Exception){
             throw e
